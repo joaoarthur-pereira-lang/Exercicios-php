93228b5e7667e2a1b4f65575665b60c205da93f9
João Arthur Pereira <joao_arthur-pereira@estudante.sesisenai.org.br>
2026-08-06 12:07:10 -0300

    add - functions

--- exercício_16.php
+++ exercício_16.php
@@ -1,8 +1,30 @@
 <?php
 
 
-function contarMaiusculas(senha){
+ function contarMaiusculas(senha) {
     const match = senha.match(/[A-Z]/g);
     return match ? match.length : 0;
 }
+
+function contarMinusculas(senha) {
+    const match = senha.match(/[a-z]/g);
+    return match ? match.length : 0;
+}
+
+function contarNumeros(senha) {
+    const match = senha.match(/[0-9]/g);
+    return match ? match.length : 0;
+}
+
+function contarCaracteresEspeciais(senha) {
+    const match = senha.match(/[^a-zA-Z0-9]/g);
+    return match ? match.length : 0;
+}
+
+function obterTamanhoSenha(senha) {
+    return senha.length;
+}
+
+function classificarSegurança
+
 
